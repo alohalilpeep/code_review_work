dc8bd3e01541fed53a3546c3b80c9c1b4cf922d8
alohalilpeep <alohalilpeep@gmail.com>
2025-05-20 22:26:27 +0300

    add impl + README.md

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,17 +1,27 @@
 public class Calculator {
-    public int add(int a, int b){
-        //TODO inser your realisation in method add
+    public int add(int a, int b) {
+        return a + b;
     }
-    public int dif(int a, int b){
-        //TODO inser your realisation in method dif
+
+    public int dif(int a, int b) {
+        return a - b;
     }
-    public int div(int a, int b){
-        //TODO inser your realisation in method div
+
+    public int div(int a, int b) {
+        if (b == 0) {
+            throw new ArithmeticException("Division by zero");
+        }
+        return a / b;
     }
-    public int times(int a, int b){
-        //TODO inser your realisation in method times
+
+    public int times(int a, int b) {
+        return a * b;
     }
-    public int solver(){
-        //TODO inser your realisation in method solver
+
+    public int solver() {
+        int step1 = add(2, 3);
+        int step2 = dif(5, 1);
+        int step3 = times(step1, step2);
+        return div(step3, 2);
     }
 }
--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,31 +1,49 @@
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 class CalculatorTest {
+    private final Calculator calculator = new Calculator();
 
     @Test
     void add() {
-        //TODO inser your realisation in test-method add
+        assertEquals(5, calculator.add(2, 3), "2 + 3 should equal 5");
+        assertEquals(0, calculator.add(0, 0), "0 + 0 should equal 0");
+        assertEquals(-1, calculator.add(2, -3), "2 + (-3) should equal -1");
+        assertEquals(Integer.MAX_VALUE - 1, calculator.add(Integer.MAX_VALUE - 2, 1),
+                "Edge case near MAX_VALUE");
     }
 
     @Test
     void dif() {
-        //TODO inser your realisation in test-method dif
+        assertEquals(1, calculator.dif(3, 2), "3 - 2 should equal 1");
+        assertEquals(-5, calculator.dif(0, 5), "0 - 5 should equal -5");
+        assertEquals(0, calculator.dif(10, 10), "10 - 10 should equal 0");
+        assertEquals(Integer.MIN_VALUE + 1, calculator.dif(Integer.MIN_VALUE + 2, 1),
+                "Edge case near MIN_VALUE");
     }
 
     @Test
     void div() {
-        //TODO inser your realisation in test-method div
+        assertEquals(2, calculator.div(4, 2), "4 / 2 should equal 2");
+        assertEquals(0, calculator.div(0, 5), "0 / 5 should equal 0");
+        assertEquals(-3, calculator.div(9, -3), "9 / (-3) should equal -3");
+
+        assertThrows(ArithmeticException.class, () -> calculator.div(5, 0),
+                "Division by zero should throw ArithmeticException");
     }
 
     @Test
     void times() {
-        //TODO inser your realisation in test-method times
+        assertEquals(6, calculator.times(2, 3), "2 * 3 should equal 6");
+        assertEquals(0, calculator.times(0, 5), "0 * 5 should equal 0");
+        assertEquals(-15, calculator.times(3, -5), "3 * (-5) should equal -15");
+        assertEquals(Integer.MAX_VALUE, calculator.times(Integer.MAX_VALUE, 1),
+                "Edge case with MAX_VALUE");
     }
 
     @Test
     void solver() {
-        //TODO inser your realisation in test-method solver
+        // Testing the example calculation: (2 + 3) * (5 - 1) / 2 = 10
+        assertEquals(10, calculator.solver(), "solver() should return 10 for the implemented calculation");
     }
 }
